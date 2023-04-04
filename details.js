4eb4b3e5515220e2b596f976ac6e5a56b5444cff
vamshi-krishna-vijayagiri <125733221+vamshi-krishna-vijayagiri@users.noreply.github.com>
2023-04-05 00:23:25 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,31 +1,25 @@
-document.getElementById("uname").textContent = localStorage.getItem("name")
-document.getElementById("uweight").textContent = localStorage.getItem("weight")
-document.getElementById("ubtype").textContent = localStorage.getItem("btype")
-document.getElementById("ub").textContent = localStorage.getItem("gender")
+document.getElementById("uname").textContent = localStorage.getItem("name");
+document.getElementById("uweight").textContent = localStorage.getItem("weight");
+document.getElementById("bodytype").textContent = localStorage.getItem("bodytype");
+document.getElementById("gender").textContent = localStorage.getItem("gender");
 
-
-
-
-const NoDiseaseSelected  = document.getElementById("NoDiseaseSelected")
-
-
+const NoDiseaseSelected  = document.getElementById("NoDiseaseSelected");
 
 function navigateToPage() {
-    localStorage.removeItem("diseases")
+    localStorage.removeItem("diseases");
     window.location.href = "optionspage.html";
 }
-const diseaseArray  = localStorage.getItem("diseases").trim().split(" ")
 
-console.log(diseaseArray.length)
+const diseaseArray  = localStorage.getItem("diseases").trim().split(" ");
 
 if(diseaseArray.length === 1 && diseaseArray[0]==="")
 {
-    NoDiseaseSelected.textContent = "Ahh, It seems like you dont have any of mentioned conditions"
+    NoDiseaseSelected.textContent = "Ahh, It seems like you dont have any of the mentioned conditions";
 }
-console.log(NoDiseaseSelected)
-const arrayOfNine = ["1","2","3","4","5","6","7","8","9"]
-arrayOfNine.forEach( number =>{
-    console.log(diseaseArray)
+
+const arrayOfNine = ["1","2","3","4","5","6","7","8","9"];
+
+arrayOfNine.forEach( number => {
     if(diseaseArray.find( di => di===number))
     {
         document.getElementById(`d${number}`).style.display ="inline";
@@ -34,5 +28,5 @@ arrayOfNine.forEach( number =>{
     {
         document.getElementById(`d${number}`).style.display ="none";
     }
-})
+});
 
